e0a5c9bb803de4d61a074368d0850ad22dbb2e23
luckydipper <qhrqufdlek@khu.ac.kr>
2023-12-08 23:12:56 +0900

    [ADD] - hamming distance function

--- src/main.cpp
+++ src/main.cpp
@@ -14,28 +14,56 @@ using namespace std;
 // macher 
 
 
+namespace bottom_up{
+    int hammingDistance(const uchar code1, const uchar code2){
+        int result = 0;
+        int XOR = (code1 ^ code2);
+        while(XOR >0){
+            if(XOR%2 > 0)
+                result++;
+            XOR >>= 1;
+        }
+        return result;
+    }
 
-vector<DMatch> featureMatcher(const Mat &img1, const vector<KeyPoint> &keypoints_1, const Mat &img2, const vector<KeyPoint> &keypoints_2 ){
-    Ptr<FeatureDetector> detector = ORB::create();
-    Ptr<DescriptorExtractor> descriptor = ORB::create();
+    
 
-    detector->detect(img1)
-    descriptor->compute()
 }
+
 
 
 int main(){
     // Load ten images 
-    Mat imgs[10];
-    for(int i=0; i < 10; i++){
+    Mat imgs[11];
+    vector<KeyPoint> keypoints[11];
+    Mat descriptors[11];
+    int end_img_index = 3;
+
+    for(int i=1; i < end_img_index; i++){
+        // Load imgs
+        string src("../imgs/");
         string index = to_string(i);
         string extension(".jpg");
-        string img_name = extension + index;
-        imgs[i] = imread(img_name, IMREAD_COLOR);
-        assert(!imgs[i].data); //image load error
-        imshow(img_name, imgs[i]);   
-
+        string img_path = src + index + extension;
+        cout << img_path; 
+        imgs[i] = imread(img_path, IMREAD_COLOR);
+        assert(imgs[i].data); 
+        
+        // detect and descript the imgs
+        Ptr<FeatureDetector> detector = ORB::create(); // by default make 500 features
+        Ptr<DescriptorExtractor> descriptor = ORB::create(); // by default 32 bits per one keypoint. 
+        detector->detect(imgs[i],keypoints[i]);
+        descriptor->compute(imgs[i],keypoints[i],descriptors[i]); // 256bit,  256 pair of points 256X2 points 32=> 8bit*32 bitmap
+        cout << descriptors[i].size() << "\n\n";   
     }
-    
-    waitKey();
+
+    //BF Matcher implementation 
+
+
+
+
+    // test code
+    // cout << bottom_up::hammingDistance(128,5);
+    //imshow(img_path, imgs[i]); 
+    //waitKey();
 }
