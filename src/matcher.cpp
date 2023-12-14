b9351f6dffbaa759f4310c786db42bb09a8b2be3
luckydipper <qhrqufdlek@khu.ac.kr>
2023-12-14 12:36:25 +0900

    [CAUTION] - before commit will not executable Becuase CMakeLists arnot updated

--- src/matcher.cpp
+++ src/matcher.cpp
@@ -31,4 +31,43 @@ namespace bottom_up{
         return result_match;
     }
 
+
+    // TODO : select good feature
+
+	// double dMaxDist = matches[test_idx_l][test_idx_r][0].distance;
+	// double dMinDist = matches[test_idx_l][test_idx_r][0].distance;
+	// double dDistance;
+
+	// // 두 개의 keypoint 사이에서 min-max를 계산한다 (min값만 사용)
+	// for (int i = 0; i < descriptors[test_idx_l].rows; i++) {
+	// 	dDistance = matches[test_idx_l][test_idx_r][i].distance;
+
+	// 	if (dDistance < dMinDist) dMinDist = dDistance;
+	// 	if (dDistance > dMaxDist) dMaxDist = dDistance;
+	// }
+	// printf("max_dist : %f \n", dMaxDist);
+	// printf("min_dist : %f \n", dMinDist);
+
+	// //match의 distance 값이 작을수록 matching이 잘 된 것
+	// //min의 값의 3배 또는 good_matches.size() > 60 까지만 goodmatch로 인정해준다.
+	// vector<DMatch>good_matches;
+	// int distance = 3;
+	// do {
+	// 	vector<DMatch>good_matches2;
+	// 	for (int i = 0; i < descriptors[test_idx_l].rows; i++) {
+	// 		if (matches[test_idx_l][test_idx_r][i].distance < distance * dMinDist)
+	// 			good_matches2.push_back(matches[test_idx_l][test_idx_r][i]);
+	// 	}
+	// 	good_matches = good_matches2;
+	// 	distance -= 1;
+	// } while (distance != 2 && good_matches.size() > 60);
+
+    // Mat matGoodMatches;
+    // drawMatches(imgs[test_idx_l], keypoints[test_idx_l], imgs[test_idx_r], keypoints[test_idx_r], good_matches, matGoodMatches, Scalar::all(-1), Scalar::all(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+    // resize(matGoodMatches,matGoodMatches, Size(matGoodMatches.cols/4,matGoodMatches.rows/4));
+
+    // imshow("-1", matGoodMatches );
+    // waitKey();
+
+    // cout << matches[test_idx_l][test_idx_r].size();
 }
