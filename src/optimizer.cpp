965c6ff2e57d1d42635fcbcf4a91305ddea31c58
luckydipper <qhrqufdlek@khu.ac.kr>
2023-12-23 16:59:26 +0900

    [ADD] - gauss newton method. NOT-TESTED

--- src/optimizer.cpp
+++ src/optimizer.cpp
@@ -56,7 +56,7 @@ cv::Mat computeHomographyDLT(const std::vector<cv::KeyPoint>& source,
     
     assert(source.size() == destination.size() && matches.size() >= 4);
 
-    // Construct matrix A
+    // Jacobian 
     Eigen::MatrixXd A(2 * matches.size(), 9);
     for(int i = 0; i < matches.size(); ++i){
         Point2d src_point = source[matches[i].here].pt, dst_point = destination[matches[i].there].pt;
@@ -99,6 +99,193 @@ cv::Mat computeHomographyDLT(const std::vector<cv::KeyPoint>& source,
 
     return H;
 }
+
+
+
+cv::Mat computeHomographyGaussNewton(const std::vector<cv::KeyPoint>& source, 
+                                     const std::vector<cv::KeyPoint>& destination,
+                                     const vector<bottom_up::FeatureMapping>& matches,
+                                     const Mat& current_homography) {
+    
+
+    int ITERATION = 100, SAMPLE_SIZE = 6, EUCLIDIAN_THRESHOLD = 3;
+    
+
+    Mat result_homography;
+    
+    int max_inlier =-999999999;// -INF
+    while(max_inlier <= 5){
+        //RANSAC 
+        vector<bottom_up::FeatureMapping> interesting_match = sampleArbitaryPoints(matches, SAMPLE_SIZE);
+
+        // Jacobian 
+        Eigen::MatrixXd A(2 * interesting_match.size(), 9);
+        for(int i = 0; i < interesting_match.size(); ++i){
+            Point2d src_point = source[interesting_match[i].here].pt, dst_point = destination[interesting_match[i].there].pt;
+            double x = src_point.x, y = src_point.y;
+            double u = dst_point.x, v = dst_point.y;
+    
+            A(2*i, 0) = x;
+            A(2*i, 1) = y;
+            A(2*i, 2) = 1;
+            A(2*i, 3) = 0;
+            A(2*i, 4) = 0;
+            A(2*i, 5) = 0;
+            A(2*i, 6) = -1*x * u;
+            A(2*i, 7) = -1*y * u;
+            A(2*i, 8) = -1*u;
+    
+            A(2*i+1, 0) = 0;
+            A(2*i+1, 1) = 0;
+            A(2*i+1, 2) = 0;
+            A(2*i+1, 3) = x;
+            A(2*i+1, 4) = y;
+            A(2*i+1, 5) = 1;
+            A(2*i+1, 6) = -1*x * v;
+            A(2*i+1, 7) = -1*y * v;
+            A(2*i+1, 8) = -1*v;
+        }
+        // Compute SVD of A
+        Eigen::JacobiSVD<Eigen::MatrixXd> svd(A, Eigen::ComputeFullV);
+        Eigen::VectorXd h = svd.matrixV().col(8);
+
+        // Convert the Eigen matrix to cv::Mat
+        Mat H = Mat::zeros(3, 3, CV_64F);
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                H.at<double>(i, j) = h(i * 3 + j) / h(8);
+            }
+        }
+        vector<KeyPoint> query_points, gt_points;
+        for (const auto& m : interesting_match){
+            query_points.push_back(source[m.here]);
+            gt_points.push_back(destination[m.there]);
+        }
+        int inlier = countInlier(query_points, gt_points, H, 3.);
+        
+        if(inlier > max_inlier){
+            max_inlier = inlier;
+            //cout << max_inlier << "\n";
+            result_homography = H;
+        }
+        interesting_match.clear(); 
+    }
+    //vector<bottom_up::FeatureMapping> interesting_match = sampleArbitaryPoints(matches, SAMPLE_SIZE);
+
+    return result_homography;
+}
+
+
+int countInlier(vector<KeyPoint> queries, vector<KeyPoint> ground_truthes, Mat homography, double threshold){
+    int num_inlier = 0;
+    vector<Point2d> queries_;
+    for(const KeyPoint &kp : queries)
+        queries_.push_back(Point2d(kp.pt));
+    vector<Point2d> predicts = bottom_up::getTransformedPoints(homography, queries_);//std::vector<cv::Point2d> &original_points)
+
+    for(int i = 0; i < predicts.size(); i++){
+        double dx = predicts[i].x - ground_truthes[i].pt.x;
+        double dy = predicts[i].y - ground_truthes[i].pt.y;
+        double distance = sqrt(dx*dx + dy*dy);
+
+        if(distance < threshold)
+            num_inlier++;
+    }
+    return num_inlier;
+}
+
+
+vector<bottom_up::FeatureMapping> sampleArbitaryPoints(const vector<bottom_up::FeatureMapping>& matches, int sample_size){
+    random_device seed;
+    mt19937 gen(seed());
+    uniform_int_distribution<> dist(0, matches.size());
+    vector<bottom_up::FeatureMapping> interesting_match;
+    for(int i = 0; i < sample_size; i++){
+        int index = dist(gen);
+        interesting_match.push_back(matches[index]);
+    }
+    return interesting_match;
+} 
+
+///////////////////////////////////////////////////////////////////
+
+// Function to convert cv::Mat to Eigen::MatrixXd
+Eigen::MatrixXd cvMatToEigen(const cv::Mat &cvMat) {
+    Eigen::MatrixXd eigenMat(cvMat.rows, cvMat.cols);
+    for (int i = 0; i < cvMat.rows; ++i) {
+        for (int j = 0; j < cvMat.cols; ++j) {
+            eigenMat(i, j) = cvMat.at<double>(i, j);
+        }
+    }
+    return eigenMat;
+}
+
+// Function to apply homography to a point
+Point2d applyHomography(const Eigen::MatrixXd &H, const Point2d &pt) {
+    Eigen::Vector3d pt_homogeneous(pt.x, pt.y, 1.0);
+    Eigen::Vector3d transformed_pt = H * pt_homogeneous;
+    return Point2d(transformed_pt(0) / transformed_pt(2), transformed_pt(1) / transformed_pt(2));
+}
+
+// Function to compute the Jacobian matrix of the homography at a given point
+Eigen::MatrixXd computeJacobian(const Point2d &srcPt, const Eigen::MatrixXd &H) {
+    double x = srcPt.x;
+    double y = srcPt.y;
+    Eigen::Vector3d pt_homogeneous(x, y, 1.0);
+    Eigen::Vector3d transformed_pt = H * pt_homogeneous;
+
+    double u = transformed_pt(0);
+    double v = transformed_pt(1);
+    double w = transformed_pt(2);
+
+    Eigen::MatrixXd J(2, 8);
+    J << x/w, y/w, 1.0/w, 0.0, 0.0, 0.0, -x*u/(w*w), -y*u/(w*w),
+         0.0, 0.0, 0.0, x/w, y/w, 1.0/w, -x*v/(w*w), -y*v/(w*w);
+    return J;
+}
+
+// Gauss-Newton optimization to refine the homography matrix
+Eigen::MatrixXd refineHomographyGaussNewton(const vector<Point2d> &srcPoints, const vector<Point2d> &dstPoints, const Eigen::MatrixXd &initialHomography, int maxIterations) {
+    Eigen::MatrixXd H = initialHomography;
+    for (int iter = 0; iter < maxIterations; ++iter) {
+        Eigen::MatrixXd J; // Overall Jacobian matrix
+        Eigen::VectorXd r; // Overall residual vector
+
+        for (size_t i = 0; i < srcPoints.size(); ++i) {
+            Point2d transformed_pt = applyHomography(H, srcPoints[i]);
+            Eigen::MatrixXd J_i = computeJacobian(srcPoints[i], H); // Jacobian for this point
+
+            // Residual for this point
+            Eigen::VectorXd r_i(2);
+            r_i << transformed_pt.x - dstPoints[i].x, transformed_pt.y - dstPoints[i].y;
+
+            // Append to overall Jacobian and residual
+            J.conservativeResize(J.rows() + J_i.rows(), Eigen::NoChange);
+            J.bottomRows(J_i.rows()) = J_i;
+            r.conservativeResize(r.size() + r_i.size());
+            r.tail(r_i.size()) = r_i;
+        }
+
+        // Compute the update
+        Eigen::VectorXd delta = (J.transpose() * J).ldlt().solve(-J.transpose() * r);
+
+        // Update the homography matrix
+        Eigen::MatrixXd deltaMat = Eigen::MatrixXd::Identity(3, 3);
+        deltaMat(0, 0) += delta(0);
+        deltaMat(0, 1) += delta(1);
+        deltaMat(0, 2) += delta(2);
+        deltaMat(1, 0) += delta(3);
+        deltaMat(1, 1) += delta(4);
+        deltaMat(1, 2) += delta(5);
+        deltaMat(2, 0) += delta(6);
+        deltaMat(2, 1) += delta(7);
+
+        H = H * deltaMat;
+    }
+    return H;
+}
+
+
 
 
 
--- src/optimizer.hpp
+++ src/optimizer.hpp
@@ -1,17 +1,37 @@
+#ifndef ___optimizer___
+#define ___optimizer___
 #include <opencv2/core.hpp>
+//#include <opencv2/core/eigen.hpp>
 #include <cmath>
+#include <random>
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/SVD>
 #include "matcher.hpp"
+#include "stitcher.hpp" // to use get transformed point
 
 using namespace std;
 using namespace cv;
+using namespace Eigen;
 
 //double f1, theta11, theta12, theta13, f2, theta21, theta22, theta23;
 namespace bottom_up{
+
 Mat getIntrinsicMatrix(double focal_length);
 Mat getRotationMatrix(double theta1, double theta2, double theta3);
 cv::Mat computeHomographyDLT(const std::vector<cv::KeyPoint>& source, 
                              const std::vector<cv::KeyPoint>& destination,
                              const vector<bottom_up::FeatureMapping>& matches);
+cv::Mat computeHomographyGaussNewton(const std::vector<cv::KeyPoint>& source, 
+                                     const std::vector<cv::KeyPoint>& destination,
+                                     const vector<bottom_up::FeatureMapping>& matches,
+                                     const Mat& current_homography);
+int countInlier(vector<KeyPoint> queries, vector<KeyPoint> ground_truthes, Mat homography, double threshold);
+Eigen::MatrixXd cvMatToEigen(const cv::Mat &cvMat);
+Point2d applyHomography(const Eigen::MatrixXd &H, const Point2d &pt);
+vector<bottom_up::FeatureMapping> sampleArbitaryPoints(const vector<bottom_up::FeatureMapping>& matches, int sample_size);
+Eigen::MatrixXd computeJacobian(const Point2d &srcPt, const Eigen::MatrixXd &H);
+Eigen::MatrixXd refineHomographyGaussNewton(const vector<Point2d> &srcPoints, const vector<Point2d> &dstPoints, const Eigen::MatrixXd &initialHomography, int maxIterations);
+
 }
+
+#endif
